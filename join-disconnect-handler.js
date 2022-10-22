7f78402ff26ddc2f7d544dda7f61dae92e86681f
Covenant Okoroafor <okoroaforcovenant99@gmail.com>
2022-10-22 12:58:35 +0100

    Fixed the bug with sockets triggering 'undefined' usernames on disconnect

--- join-disconnect-handler.js
+++ join-disconnect-handler.js
@@ -1,13 +1,15 @@
 module.exports = (io, people) => {
-    const join = function(username) {
+    const join = function (username) {
         const socket = this;
         people[socket.id] = username;
         socket.broadcast.emit('connection/disconnection message', `Update: ${username} joined the chat. Say hi!`);
     };
 
-    const disconnect = function() {
+    const disconnect = function () {
         const socket = this;
-        io.emit('connection/disconnection message', "Update: " + people[socket.id] + " left the chat!")
-    }
-    return {join, disconnect}
+        if (people[socket.id]) {
+            io.emit('connection/disconnection message', "Update: " + people[socket.id] + " left the chat!");
+        };
+    };
+    return { join, disconnect }
 } 
